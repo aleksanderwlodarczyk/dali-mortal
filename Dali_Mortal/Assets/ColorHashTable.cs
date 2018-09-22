300517eb44b4ccb7c6958de8d8170dc40a8b1c5c
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-09-22 15:38:00 +0200

    we got this / hashtables of colors

---
+++ Dali_Mortal/Assets/ColorHashTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHashTable : MonoBehaviour {
+
+	public Hashtable colorHashtable = new Hashtable();
+	public int count;
+
+	private void Update()
+	{
+		count = colorHashtable.Count;
+	}
+}
--- Dali_Mortal/Assets/ColorShiftScript2.cs
+++ Dali_Mortal/Assets/ColorShiftScript2.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,13 +13,44 @@ public class ColorShiftScript2 : MonoBehaviour
 	public double TestLength = 550.0f;
 	public int numberOfSkipedColors;
 	public Color32 TestColor;
+
+	public Color32 color1;
+	public Color32 color2;
+
+	public int tries = 0;
+
+	public ColorHashTable colorHashTablePrefab;
 	void Start()
 	{
 		Vector3 VecColor = new Vector3(TestColor.r, TestColor.g, TestColor.b);  //convert_wave_length_nm_to_rgb(TestLength);
 
 		//double Wave = ApproxColorToWave(ref VecColor);
 		
-		Debug.Log("Approx Wave " + ApproxColorToWave(ref VecColor));
+		//Debug.Log("Approx Wave " + ApproxColorToWave(ref VecColor));
+
+		if (colorHashTablePrefab)
+		{
+			GenerateColorDictionary();
+			colorHashTablePrefab.colorHashtable = colorTable;
+		}
+
+		Vector3 color1vec = new Vector3(color1.r, color1.g, color1.b);
+		Vector3 color2vec = new Vector3(color2.r, color2.g, color2.b);
+
+		int[] color1lab = rgb2lab((int)color1vec.x, (int)color1vec.y, (int)color1vec.z);
+		int[] color2lab = rgb2lab((int)color2vec.x, (int)color2vec.y, (int)color2vec.z);
+
+
+
+		int color1LabInt = color1lab[0] * color1lab[1] * color1lab[2];
+		int color2LabInt = color2lab[0] * color2lab[1] * color2lab[2];
+
+		double difference = GetColorDifference(color1lab, color2lab);
+		Debug.Log(color1lab[0] + " " + color1lab[1] + " " + color1lab[2]);
+		Debug.Log(color2lab[0] + " " + color2lab[1] + " " + color2lab[2]);
+
+		Debug.Log(difference);
+
 	}
 
 	// Update is called once per frame
@@ -28,39 +60,76 @@ public class ColorShiftScript2 : MonoBehaviour
 
 	void GenerateColorDictionary()
 	{
-		Vector3 CurrentColor = new Vector3(numberOfSkipedColors, numberOfSkipedColors, numberOfSkipedColors);
-		for (int i = 0; i < maxNumberOfColors / numberOfSkipedColors; i++)
-		{
-			float fi = (float)i + 1;
-			Vector3[] ArrayOfVec = new Vector3[7];
-			ArrayOfVec[0] = Vector3.Scale(CurrentColor, new Vector3(fi, 0.0f, 0.0f));
-			ArrayOfVec[1] = Vector3.Scale(CurrentColor, new Vector3(0.0f, fi, 0.0f));
-			ArrayOfVec[2] = Vector3.Scale(CurrentColor, new Vector3(0.0f, 0.0f, fi));
-			ArrayOfVec[3] = Vector3.Scale(CurrentColor, new Vector3(fi, fi, 0.0f));
-			ArrayOfVec[4] = Vector3.Scale(CurrentColor, new Vector3(fi, 0.0f, fi));
-			ArrayOfVec[5] = Vector3.Scale(CurrentColor, new Vector3(0.0f, fi, fi));
-			ArrayOfVec[6] = Vector3.Scale(CurrentColor, new Vector3(fi, fi, fi));
+		Vector3 CurrentColor = new Vector3(1.0f, 1.0f, 1.0f);
+		Vector3[] ArrayOfVec = new Vector3[7];
+		int Index1 = 1;
+		int Index2 = 1;
+		int Index3 = 1;
 
-			for (int Index = 0; Index < 7; ++Index)
+		float r = 0;
+		float g = 0;
+		float b = 0;
+
+		for (; Index1 <= 255; Index1 +=numberOfSkipedColors)
+		{
+			r = Index1;
+			g = 0;
+			Index2 = 0;
+			for (; Index2 <= 255; Index2 += numberOfSkipedColors)
 			{
-				int Hash = Mathf.FloorToInt((2 * ArrayOfVec[Index].x) + (4 * ArrayOfVec[Index].y) + (8 * ArrayOfVec[Index].z));
-				colorTable.Add(Hash, ApproxColorToWave(ref ArrayOfVec[Index]));
+				g = Index2;
+				b = 0;
+				Index3 = 0;
+				for (; Index3 <= 255; Index3 += numberOfSkipedColors)
+				{
+					b = Index3;
+					Vector3 NewColor = new Vector3(r, g, b);
+			
+					string Hash = String.Format("{0}{1}{2}", X((int)NewColor.x) + NewColor.x, X((int)NewColor.y) + NewColor.y, X((int)NewColor.z) + NewColor.z);
+					++tries;
+					if (!colorTable.ContainsKey(Hash))
+					{
+						colorTable.Add(Hash, ApproxColorToWave(ref NewColor));
+					}
+				}
 			}
+		}
+	}
+
+	string X (int Number)
+	{
+		if (Number / 100 > 0)
+		{
+			return "";
+		}
+		else if (Number / 10 > 10)
+		{
+			return "0";
+		}
+		else
+		{
+			return "00";
 		}
 	}
 
 	double ApproxColorToWave(ref Vector3 inColor)
 	{
-		float CurrentMin = 600000; // max enough
-		double ReturnWave = 0;
-		for (int Wave = 400; Wave <= 700; ++Wave)
+		double CurrentMin = 600000d; // max enough
+		double ReturnWave = 0d; double min = 0d;
+		for (int Wave = 380; Wave <= 780; ++Wave)
 		{
 			Vector3 color = convert_wave_length_nm_to_rgb(Wave);
-			float a = Mathf.Abs(inColor.x - color.x);
-			float b = Mathf.Abs(inColor.y - color.y);
-			float c = Mathf.Abs(inColor.z - color.z);
+			//float a = Mathf.Abs(inColor.x - color.x);
+			//float b = Mathf.Abs(inColor.y - color.y);
+			//float c = Mathf.Abs(inColor.z - color.z);
 
-			float min = a + b + c;
+			int[] labIn = rgb2lab((int)inColor.x, (int)inColor.y, (int)inColor.z);
+			int[] labMap = rgb2lab((int)color.x, (int)color.y, (int)color.z);
+
+			int inColorLabInt = labIn[0] * labIn[1] * labIn[2];
+			int mapColorLabInt = labMap[0] * labMap[1] * labMap[2];
+
+			min = GetColorDifference(labIn, labMap);
 
 			if (min < CurrentMin)
 			{
@@ -68,9 +137,94 @@ public class ColorShiftScript2 : MonoBehaviour
 				CurrentMin = min;
 			}
 		}
+		Debug.Log(min);
+		int lol;
 		return ReturnWave;
 	}
-Vector3 convert_wave_length_nm_to_rgb(double wave_length_nm)
+
+	public int[] rgb2lab(int R, int G, int B)
+	{
+		float r, g, b, X, Y, Z, fx, fy, fz, xr, yr, zr;
+		float Ls, As, bs;
+		float eps = 216f / 24389f;
+		float k = 24389f / 27f;
+
+		float Xr = 0.964221f;  // reference white D50
+		float Yr = 1.0f;
+		float Zr = 0.825211f;
+
+		// RGB to XYZ
+		r = R / 255f; //R 0..1
+		g = G / 255f; //G 0..1
+		b = B / 255f; //B 0..1
+
+		// assuming sRGB (D65)
+		if (r <= 0.04045)
+			r = r / 12;
+		else
+			r = (float)System.Math.Pow((r + 0.055) / 1.055, 2.4);
+
+		if (g <= 0.04045)
+			g = g / 12;
+		else
+			g = (float)System.Math.Pow((g + 0.055) / 1.055, 2.4);
+
+		if (b <= 0.04045)
+			b = b / 12;
+		else
+			b = (float)System.Math.Pow((b + 0.055) / 1.055, 2.4);
+
+
+		X = 0.436052025f * r + 0.385081593f * g + 0.143087414f * b;
+		Y = 0.222491598f * r + 0.71688606f * g + 0.060621486f * b;
+		Z = 0.013929122f * r + 0.097097002f * g + 0.71418547f * b;
+
+		// XYZ to Lab
+		xr = X / Xr;
+		yr = Y / Yr;
+		zr = Z / Zr;
+
+		if (xr > eps)
+			fx = (float)System.Math.Pow(xr, 1 / 3d);
+		else
+			fx = (float)((k * xr + 16d) / 116d);
+
+		if (yr > eps)
+			fy = (float)System.Math.Pow(yr, 1 / 3d);
+		else
+			fy = (float)((k * yr + 16d) / 116d);
+
+		if (zr > eps)
+			fz = (float)System.Math.Pow(zr, 1 / 3d);
+		else
+			fz = (float)((k * zr + 16d) / 116);
+
+		Ls = (116 * fy) - 16;
+
+		As = 500 * (fx - fy);
+		bs = 200 * (fy - fz);
+
+		int[] lab = new int[3];
+		lab[0] = (int)(/*2.55 **/ Ls + .5);
+		lab[1] = (int)(As + .5);
+		lab[2] = (int)(bs + .5);
+		return lab;
+	}
+
+	public double GetColorDifference(int[] lab1, int[] lab2)
+	{
+		//int r1, g1, b1, r2, g2, b2;
+		//r1 = (a >> 16) & 0xFF;
+		//g1 = (a >> 8) & 0xFF;
+		//b1 = a & 0xFF;
+		//r2 = (b >> 16) & 0xFF;
+		//g2 = (b >> 8) & 0xFF;
+		//b2 = b & 0xFF;
+		//int[] lab1 = rgb2lab(r1, g1, b1);
+		//int[] lab2 = rgb2lab(r2, g2, b2);
+		return System.Math.Sqrt(System.Math.Pow(lab2[0] - lab1[0], 2) + System.Math.Pow(lab2[1] - lab1[1], 2) + System.Math.Pow(lab2[2] - lab1[2], 2));
+	}
+	Vector3 convert_wave_length_nm_to_rgb(double wave_length_nm)
 {
    // Credits: Dan Bruton http://www.physics.sfasu.edu/astro/color.html
    double red   = 0.0;
