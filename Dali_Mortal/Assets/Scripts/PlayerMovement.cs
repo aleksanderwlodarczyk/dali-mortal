8f828551fd67954d7abe2f8cf305c9b323ff366d
Sharpshark <maciej.jasielewicz@gmail.com>
2018-09-22 05:59:20 +0200

    Add Camera movement

--- Dali_Mortal/Assets/Scripts/PlayerMovement.cs
+++ Dali_Mortal/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public float player_X_Speed = 3f;
+    public float player_Y_Speed = 3f;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,10 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        float wantedPlayer_X = Input.GetAxis("Horizontal") * player_X_Speed;
+        float wantedPlayer_Y = Input.GetAxis("Vertical") * player_Y_Speed;
+
+        transform.Translate(wantedPlayer_X,wantedPlayer_Y,0);
 	}
 }
---
+++ Dali_Mortal/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+    
+    public Transform target;
+
+    Vector3 velocity = Vector3.zero;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+	void LateUpdate()
+    {
+        if (target)
+        {
+            
+            Vector3 desiredPosition = target.position + offset;
+
+            Vector3 FinalPos = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+            transform.position = FinalPos;
+
+            transform.LookAt(target);
+
+
+        }
+    }
+}
