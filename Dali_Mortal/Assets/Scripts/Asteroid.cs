26eb25102b7e04967328c15e50da384a2b209b2a
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-09-22 04:42:27 +0200

    added asteroid script / near miss handler

---
+++ Dali_Mortal/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	private bool nearMissPossible;
+
+	void Start () {
+		nearMissPossible = false;
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			nearMissPossible = true;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+			nearMissPossible = false;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player" && nearMissPossible)
+		{
+			// near miss done, add points, show text or something
+			Debug.Log("near miss!");
+		}
+	}
+}
